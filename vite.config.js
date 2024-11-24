5870617be6a154c8086b56086b1482fef1bb4b9b
hqp001 <hqp001@bucknell.edu>
2024-11-24 08:30:04 -0500

    change vite settings

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: 'Hyperbolic-interactive',
   root: 'src', // Use 'src' as the project root
   build: {
     outDir: '../dist', // Output files to a 'dist' folder outside 'src'
